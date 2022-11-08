2e1d944d5b6f6fa9c823a2e45e136ff63d69c9ab
Crisano97 <97keb6@gmail.com>
2022-11-08 13:31:58 +0100

    init login page + init config

--- index.php
+++ index.php
@@ -6,10 +6,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edusogno</title>
+    <link rel="stylesheet" href="./assets/styles/style.css">
 </head>
 
 <body>
-
+    <header>
+        <img class="logo" src="./assets/images/logo-edusogno.png" alt="edusogno-logo">
+    </header>
+    <main>
+        <div class="container">
+            <form action="">
+                <div class="input-container">
+                    <label class="d-block" for="name">Inserici il nome</label>
+                    <input class="d-block custom-input" type="text" name="name" id="name" placeholder="Mario">
+                </div>
+                <div class="input-container">
+                    <label class="d-block" for="surname">Inserici il cognome</label>
+                    <input class="d-block custom-input" type="text" name="surname" id="surname" placeholder="Rossi">
+                </div>
+                <div class="input-container">
+                    <label class="d-block" for="email">Inserici l'email</label>
+                    <input class="d-block custom-input" type="email" name="email" id="email" placeholder="name@example.com">
+                </div>
+                <div class="input-container">
+                    <label class="d-block" for="password">Inserici la password</label>
+                    <input class="d-block custom-input" type="text" name="password" id="password" placeholder="Scrivila qui">
+                </div>
+                <div class="button-container">
+                    <button class="btn" type="submit">REGISTRATI</button>
+                </div>
+                <div class="link-container">
+                    <a href="#">Hai giá un account? <b>Accedi</b></a>
+                </div>
+            </form>
+        </div>
+    </main>
 </body>
 
 </html>
